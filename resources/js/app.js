b6798c12758d80c8ea7a5f8211e9916d4f1e2417
Maty2002v2 <matikster21@gmail.com>
2025-02-11 13:40:33 +0100

    Adding bootstrap to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
