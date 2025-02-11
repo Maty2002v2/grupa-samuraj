8cd8a3ca4608fa9c7ea5f93c08f15982b087730a
Maty2002v2 <matikster21@gmail.com>
2025-02-11 16:02:10 +0100

    Expanding the index page

--- resources/js/Pages/IndexArticle.vue
+++ resources/js/Pages/IndexArticle.vue
@@ -3,19 +3,23 @@ const props = defineProps(['articles']);
 </script>
 
 <template>
-    <div>
+    <div class="container">
         <h1>Articles</h1>
 
-        <section>
-            <h2>Add article</h2>
-            <a :href="route('articles.create')">new</a>
+        <section class="d-flex mt-5">
+            <h2 class="m-0">Add article</h2>
+            <a :href="route('articles.create')" class="btn btn-md btn-primary ms-3">new</a>
         </section>
 
-        <section>
+        <section class="mt-5">
             <h2>Our articles</h2>
-            <article v-for="article in articles">
+            <article v-for="article in articles" class="mt-3 shadow-sm p-3 mb-5 bg-light rounded">
                 <h3>{{ article.title }}</h3>
                 <p>{{ article.content }}</p>
+                <p>
+                    <button class="btn btn-sm btn-primary">Edit</button>
+                    <button class="btn btn-sm btn-danger ms-2">Delete</button>
+                </p>
             </article>
         </section>
     </div>
